18e1fe0281280c3d548c275f863661c1f52e6fd1
ringi <mitya.info@gmail.com>
2020-11-17 19:29:21 +0200

    vue1

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,103 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
+  <div>
+<!--    <div class="demo" @click="attached =!attached"></div>-->
+<!--    <div class="demo" :class="{red:attached, blue: !attached}"></div>-->
+<!--    <div class="demo" :class="color"></div>-->
+<!--    <div class="demo" :style="{backgroundColor:color}"></div>-->
+<!--    <div class="demo" :style="myClasses"></div>-->
+<!--    <input type="text" v-model="color">-->
+<!--    <input type="text" v-model="width">-->
+<!--    <div v-if="show">-->
+<!--      <p> you can see me</p>-->
+<!--    </div>-->
+<!--    <div v-else>-->
+<!--      <p>Now you can see mee!</p>-->
+<!--    </div>-->
+<!--    <button @click="show = !show">switch text</button>-->
+<!--  -->
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li v-for="(ingridient, i) in ingridients" :key="i">
+        {{ingridient}}
+      </li>
     </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+
+<ul>
+  <li v-for="(person, index) in persons" :key="index">
+    <div v-for="(value, key, i) in person" :key="i">
+      {{value}}-{{key}}
+    </div>
+  </li>
+</ul>
+
+
+
+
   </div>
+
+
+
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      title: 'history of smb:',
+      nameGirl: 'Lida',
+      nameBoy: 'Dima',
+      attached: false,
+      color:'green',
+      width: 100,
+      show:false,
+      ingridients:['meat', 'fruit', 'cookies'],
+      persons:[
+        {name:'Jake', age:29, color: 'black'},
+        {name:'Kook', age:28, color: 'red'}
+        ]
+    }
+  },
+  computed:{
+    divClasses(){
+      return{
+        red: this.attached,
+        blue: !this.attached
+      }
+    },
+    myClasses(){
+      return{
+        backgroundColor: this.color,
+        width: this.width +'px'
+      }
+    }
+  },
+
+  methods:{
+    sayHello(){
+      return this.title = 'its true?'
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
+.demo{
+  width: 100px;
+  height: 100px;
+  background-color: gray;
   display: inline-block;
-  margin: 0 10px;
+  margin: 10px;
+
 }
-a {
-  color: #42b983;
+.red{
+  background-color: red;
 }
+.yellow{
+  background-color: yellow;
+}
+.blue{
+  background-color: blue;
+}
+
 </style>
